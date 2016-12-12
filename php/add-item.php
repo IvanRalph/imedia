d69c5dbe5d6fe79e41ba2aecf3119b4774cba323
IvanRalph <ivan.ralph123@gmail.com>
2016-12-12 19:05:38 +0800

    Another attempt on fixing add - under construction

--- php/add-item.php
+++ php/add-item.php
@@ -1,23 +1,14 @@
 <?php
 	include("config.php");
-
-	$columnValue = array();
 	
-	foreach ($_POST as $key => $value) {
-	    if(empty($value)){
-	    	$value = "";
-	    }else{
-	    	$columnValue = $value;
-	    }
-	}
 
 	$category = $_POST['category'];
 
 	if($category == "camera"){
-		// insertItem($columnValue, 'camera', '', $_POST['brand'], $_POST['model'], $_POST['type'], $_POST['c-type'], $_POST['poe'], $_POST['specs'], $_POST['lens'], $_POST['price'], '', '', '', '', '', '', '');
-		print_r($columnValue);
+		$column = implode(",", $_POST);
+		insertItem($column, 'camera', '', $_POST['brand'], $_POST['model'], $_POST['type'], $_POST['c-type'], $_POST['poe'], $_POST['specs'], $_POST['lens'], $_POST['price'], '', '', '', '', '', '', '');
+		
 	}
-
 
 	function insertItem($columnValue, $category, $image, $brand, $model, $type, $cType, $poe, $specs, $lens, $price, $ports, $vcategory, $frequency, $speed, $bands, $capability, $channels){
 		include("config.php");
@@ -26,17 +17,15 @@
 		if(!$result){
 			die("Error: " . mysqli_error($conn));
 		}
-		// $columnArray = array();
+		$columnArray = array();
 		while( $row = mysqli_fetch_assoc($result)){
     		$columnArray = array($row);
 		}
 		$implodeArray = implode(',', $columnArray);
-		// $implodeValue = implode(',', $columnValue);
-		echo $implodeArray;// . " " .$implodeValue;
-		// $sql2 = "INSERT INTO ". $category ."(". $implodeArray .") VALUES(". $implodeValue .")";
-		// $result2 = mysqli_query($conn, $sql2);
-		// if(!$result2){
-		// 	die("Error: " . mysqli_error($conn));
-		// }
+		$sql2 = "INSERT INTO ". $category ."(". $implodeArray .") VALUES(". $columnValue .")";
+		$result2 = mysqli_query($conn, $sql2);
+		if(!$result2){
+			die("Error: " . mysqli_error($conn));
+		}
 	}
 ?>
